49579efb48aebaf340478d62ed966bf95c7c7c6b
Florian Fida <ffsrctr@bytecast.net>
2014-10-30 15:19:36 +0100

    schmarr

--- www/js/index.js
+++ www/js/index.js
@@ -56,6 +56,6 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        //console.log('Received Event: ' + id);
     }
 };
